3b84c2f9e4e4977b2cebfa6a4b76ac03acb5872b
Unknown <jjolula88@gmail.com>
2017-09-27 10:29:38 +0200

    Merge branch 'hotfix/JJ'

--- app/src/main/java/com/hlc/contador/MainActivity.java
+++ app/src/main/java/com/hlc/contador/MainActivity.java
@@ -1,13 +1,25 @@
 package com.hlc.contador;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private double contador = 1;
+    private TextView texto;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle estado) {
+        super.onCreate(estado);
         setContentView(R.layout.activity_main);
+        texto = (TextView) findViewById (R.id.text);
+        texto.setText (String.valueOf (contador));
+    }
+
+    // duplica el valor del contador y lo guarda en la etiqueta texto
+    public void duplica (View view) {
+        contador = contador * 2;
+        texto.setText(String.valueOf(contador));
     }
 }
